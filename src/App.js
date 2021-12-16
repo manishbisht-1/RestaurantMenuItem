3b3dd9a1d38012ede10116b735bcfa155ee4a1d3
manish <manish01.it@gmail.com>
2021-12-16 20:44:23 +0530

    restaurant app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Restaurant from './component/Basics/Restaurant';
+
+const App = () => {
+  return (
+    <div><>
+    <Restaurant/> </>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,60 @@
+const Menu=[{
+    id:1,
+    image:'images/oatmeal.jpg',
+    name:'Oat Meal',
+    category:"Breakfast",
+    price:50,
+    description:'Oatmeal is a breakfast food made from oats and liquid like water or milk.',
+},
+
+{
+    id:2,
+    image:'images/cornFlakes.jpg',
+    name:'Corn Flakes',
+    category:"Breakfast",
+    price:20,
+    description:'Corn flakes, or cornflakes, are a breakfast cereal made from toasting flakes of corn (maize). The cereal, originally made with wheat.',
+},
+
+{
+    id:3,
+    image:'images/rajmarice.jpg',
+    name:'Rajma Rice',
+    category:"Lunch",
+    price:40,
+    description:'Rajma chawal   is  kidney  beans  served  with   boiled rice.   Rajma  is  a popular   dish  in  the  Northern  states  of  India',
+},
+
+
+{
+    id:4,
+    image:'images/rotiwithmeat.jpg',
+    name:'Roti With Meat',
+    category:"Lunch",
+    price:5,
+    description:'eating roti is to break the roti by hand, using it to sop up sauce and pieces of meat from the curry.',
+},
+
+
+{
+    id:5,
+    image:'images/vegthali.jpg',
+    name:'Veg Thali',
+    category:"Dinner",
+    price:120,
+    description:' These veg thalis are a full package featuring salad, chutney, gravies, breads, dessert, and much more.',
+},
+
+
+{
+    id:6,
+    image:'images/sweet.jpg',
+    name:'sweet',
+    category:"Disert",
+    price:60,
+    description:'dfcjdbvedbvbd ve fvbuedbvdbve bfiueufvbef eu',
+},
+
+];
+
+export default Menu;
